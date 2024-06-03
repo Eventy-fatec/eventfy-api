eda46c196b4a4b1937fe140de8034f4c112cd509
Leonardo Brienza <leonardobrienzaa71@hotmail.com>
2024-06-03 19:09:29 -0300

    Created integration with Swagger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { routes } from './routes/routes.js';
+import swaggerDocs from './swagger.json' with {type: "json"};
+
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(routes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
   console.log(`Server rodando em http://localhost:${port}`);
