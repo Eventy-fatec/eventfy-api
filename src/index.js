4118b98ea50fd97b0bc33fe614c2366475490e26
Luiz Felipe <luiz.leite@linkedrh.com.br>
2024-06-18 14:59:15 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes/routes.js';
 import swaggerDocs from './swagger.json' assert {type: "json"};
-
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(routes);
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
